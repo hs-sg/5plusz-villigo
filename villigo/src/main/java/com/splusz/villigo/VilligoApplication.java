a3de23cad88ed6ae08f1020e314f2a534c4f3685
hs-sg <hasin927@gmail.com>
2025-03-12 17:58:20 +0900

    킥오프

---
+++ villigo/src/main/java/com/splusz/villigo/VilligoApplication.java
@@ -0,0 +1,13 @@
+package com.splusz.villigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilligoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilligoApplication.class, args);
+	}
+
+}
