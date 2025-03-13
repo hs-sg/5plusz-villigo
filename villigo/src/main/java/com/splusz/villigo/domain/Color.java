e1cefb686ed9f248d84dd645d367b5129391bb75
heeseong <chsohee1537@gmail.com>
2025-03-13 20:34:59 +0900

    Color domain

---
+++ villigo/src/main/java/com/splusz/villigo/domain/Color.java
@@ -0,0 +1,31 @@
+package com.splusz.villigo.domain;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "colors")
+@Getter @Setter
+@NoArgsConstructor
+public class Color {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Basic(optional = false)
+    private String name;
+
+	@ToString.Exclude @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "rental_category_id") @Basic(optional = false)
+    private RentalCategory rentalCategory;
+}
